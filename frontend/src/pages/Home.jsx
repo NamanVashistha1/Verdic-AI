9e25cc252f04083e7224a8b3b0f142328f1f0d2b
Vidhi <vidhi2108@gmail.com>
2025-01-29 15:05:44 +0530

    component added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import TestimonialCarousel from "../components/Testimonial";
 
 export default function Home() {
   return (
     <div className="text-center">
       <h2>Welcome to the Legal News App</h2>
       <p>Stay updated with the latest legal updates.</p>
+      
+      <TestimonialCarousel/>
     </div>
   );
 }
