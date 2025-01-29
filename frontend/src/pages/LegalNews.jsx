bdb33bba1c71065804b9845a497808c497e59d71
KaustubhM234 <kaustubh.h.mhatre@gmail.com>
2025-01-30 01:23:21 +0530

    legal news & queries connected

--- frontend/src/pages/LegalNews.jsx
+++ frontend/src/pages/LegalNews.jsx
@@ -7,7 +7,7 @@ export default function LegalNews() {
   return (
     <>
         <TopBar />
-      <div className="container pt-2"> 
+      <div className="container pt-2 mt-1"> 
         {/* <h2 className="text-center my-1">Latest Legal News</h2> */}
         <NewsArticle />
       </div>
